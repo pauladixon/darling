4dd8ab515d4c5fc2c3cbdc6c68137fcba5b1e3c7
paula dixon <paulapdixon@gmail.com>
2020-10-01 11:38:25 -0700

    added friend module

---
+++ src/app/friends/friends.module.ts
@@ -0,0 +1,10 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FriendsComponent } from './friends.component'
+
+@NgModule({
+  declarations: [ FriendsComponent ],
+  imports: [ BrowserModule ]
+})
+
+export class AppModule { }
