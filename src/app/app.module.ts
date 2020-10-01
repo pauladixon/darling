97d23801efcdedf122b7f26026846218689188e9
paula dixon <paulapdixon@gmail.com>
2020-10-01 11:49:23 -0700

    added friends list sub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component';
-import { FriendsComponent } from './friends/friends.component'
+import { FriendsComponent } from './friends/friends.component';
+import { FriendsListComponent } from './friends/friends-list/friends-list.component'
 
 @NgModule({
-  declarations: [ AppComponent, FriendsComponent ],
+  declarations: [ AppComponent, FriendsComponent, FriendsListComponent ],
   imports: [ BrowserModule ],
   bootstrap: [AppComponent]
 })
