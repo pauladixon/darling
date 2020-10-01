87d38c7b3b8a646b564e6621b06c462d20aa53ba
paula dixon <paulapdixon@gmail.com>
2020-10-01 11:34:52 -0700

    added data to friend component

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,3 +1,4 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface'
 import { Component, OnInit } from '@angular/core'
 
 @Component({
@@ -5,11 +6,20 @@ import { Component, OnInit } from '@angular/core'
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.css']
 })
-export class FriendsComponent implements OnInit {
 
+export class FriendsComponent implements OnInit {
+  title: string
+  people: any[]
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.title = 'friends'
+    this.people = [
+      { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
+      { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
+      { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
+      { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
+    ]
   }
 
 }
