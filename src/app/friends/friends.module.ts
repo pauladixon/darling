1a443ff26413927106d084d1d9c560235c13aeef
paula dixon <paulapdixon@gmail.com>
2020-10-01 11:58:39 -0700

    linked friends list to friends

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { FriendsListComponent } from './friends-list/friends-list.component'
 import { FriendsComponent } from './friends.component'
 
 @NgModule({
-  declarations: [ FriendsComponent ],
+  declarations: [ FriendsComponent, FriendsListComponent ],
   imports: [ CommonModule ],
   exports: [ FriendsComponent ]
 })
