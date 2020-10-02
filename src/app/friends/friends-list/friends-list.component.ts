7a8771404613220a55aca5ac6aa89d89a8224004
paula dixon <paulapdixon@gmail.com>
2020-10-02 12:15:13 -0700

    pulling in hard coded data for table

--- src/app/friends/friends-list/friends-list.component.ts
+++ src/app/friends/friends-list/friends-list.component.ts
@@ -16,6 +16,12 @@ export class FriendsListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.filteredFriends = [
+      { id: 1, name: 'joann ahn', city: 'los angeles', loveTotal: 999, friendSince: new Date(2010, 8, 10) },
+      { id: 2, name: 'tomas diaz', city: 'el sereno', loveTotal: 199, friendSince: new Date(2020, 6, 22)},
+      { id: 3, name: 'hanna denison', city: 'venice', loveTotal: 499, friendSince: new Date(2018, 2, 31)},
+      { id: 4, name: 'isa martinez', city: 'pasadena', loveTotal: 299, friendSince: new Date(2020, 3, 3)},
+    ]
 
   }
 
