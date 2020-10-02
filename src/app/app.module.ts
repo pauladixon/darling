651d9ff46f7aca11cf3ad5084ea1f60ef105e6b0
paula dixon <paulapdixon@gmail.com>
2020-10-02 11:52:14 -0700

    importing shared module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { FriendsModule } from './friends/friends.module'
+import { SharedModule } from './shared/shared.module'
 import { AppComponent } from './app.component'
 
 @NgModule({
-  imports: [ BrowserModule, FriendsModule ],
+  imports: [ BrowserModule, FriendsModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
