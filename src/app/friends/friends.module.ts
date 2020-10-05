722694b1bd424a5f34b69817cfc26e26ee4f634e
paula dixon <paulapdixon@gmail.com>
2020-10-05 12:33:48 -0700

    implemented custom capitalize pipe

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -2,14 +2,16 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
+import { SharedModule } from '../shared/shared.module'
 import { FriendsComponent } from './friends.component'
 import { FriendsListComponent } from './friends-list/friends-list.component'
 import { FilterTextboxComponent } from './friends-list/filter-textbox.component'
 
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, SharedModule ],
   declarations: [ FriendsComponent, FriendsListComponent, FilterTextboxComponent ],
   exports: [ FriendsComponent ]
 })
 
 export class FriendsModule { }
+ 
