79caa6e1e60c5c1c4f6fecc692e5b9c5579f0092
paula dixon <paulapdixon@gmail.com>
2020-10-05 16:33:29 -0700

    added structure to core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+
+import { DataService } from './data.service'
+import { SorterService } './sorter.service'
+
+@NgModule({
+    imports: [],
+    providers: []
+})
+
+export class CoreModule {}
