02d91811ccc2bb40963a483545156b51b1141212
paula dixon <paulapdixon@gmail.com>
2020-10-05 13:06:07 -0700

    bringing in data binding structure for friend filter

--- src/app/friends/friends-list/filter-textbox.component.ts
+++ src/app/friends/friends-list/filter-textbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'filter-textbox',
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core'
     `
 })
 export class FilterTextboxComponent implements OnInit {
+
+    private _filter: string
+    @Input() get filter() {
+        return this._filter
+    }
+
+    set filter(val: string) {
+        this._filter = val
+        this.changed.emit(this.filter)
+    }
+
+    @Output() changed: EventEmitter<string> = new EventEmitter<string>()
+
+    
     constructor() {}
 
     ngOnInit() {
