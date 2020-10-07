015057f1b1dde6a8b4d0188b8f1f11dcd2134d32
paula dixon <paulapdixon@gmail.com>
2020-10-07 12:22:36 -0700

    fixed typos

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from '../shared/shared.module'
 import { FriendsComponent } from './friends.component'
 import { FriendsListComponent } from './friends-list/friends-list.component'
 import { FilterTextboxComponent } from './friends-list/filter-textbox.component'
+import { FriendsRoutingModule } from './friends-routing.module'
 
 @NgModule({
-  imports: [ CommonModule, SharedModule, FormsModule ],
+  imports: [ CommonModule, SharedModule, FormsModule, FriendsRoutingModule ],
   declarations: [ FriendsComponent, FriendsListComponent, FilterTextboxComponent ],
   exports: [ FriendsComponent ]
 })
