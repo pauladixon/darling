83311005345026108654fa430a791b3c754049d4
paula dixon <paulapdixon@gmail.com>
2020-10-07 12:04:25 -0700

    added routes for friends pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-
+    { path: '', pathMatch: 'full', redirectTo: '/friends'}
+    { path: '**', pathMatch: 'full', redirectTo: '/friends'}
 ]
 
 @NgModule({
