fa86ea15fcc74b3740c711577e2be13cd05c7cfa
paula dixon <paulapdixon@gmail.com>
2020-10-07 12:06:04 -0700

    fixed typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { Router, RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/friends'}
+    { path: '', pathMatch: 'full', redirectTo: '/friends'},
     { path: '**', pathMatch: 'full', redirectTo: '/friends'}
 ]
 
