56421f152e4e3ca1b5e9de5fa8b824c57ae3577b
paula dixon <paulapdixon@gmail.com>
2020-10-07 12:18:22 -0700

    coded friends routing module

--- src/app/friends/friends-routing.module.ts
+++ src/app/friends/friends-routing.module.ts
@@ -1,3 +1,16 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { FriendsComponent } from 'friends.component''
+import { FriendsComponent } from './friends.component'
+
+const routes: Routes = [
+    { path: 'friends', component: FriendsComponent }
+]
+
+@NgModule({
+    imports: [ RouterModule.forChild(routes) ],
+    exports: [ RouterModule ]
+})
+
+export class FriendsRoutingModule {
+
+}
