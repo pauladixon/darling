b2f708b312dd7658d3082c2e46d733b4a199c978
paula dixon <paulapdixon@gmail.com>
2020-10-08 12:04:02 -0700

    brought in imports for love component

---
+++ src/app/love/love.component.ts
@@ -0,0 +1,5 @@
+import { Component, OnInit } from '@angular/core'
+import { Router, ActivatedRoute, Params } from '@angular/router'
+
+import { DataService } from '../core/data.service'
+import { IFriend, ILove, ILoveToken } from '../shared/interfaces'
