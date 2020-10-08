2aa1ec6dec89f9cd4b66bc4a8d5216200850739b
paula dixon <paulapdixon@gmail.com>
2020-10-08 12:09:18 -0700

    added decorators and class to love component

--- src/app/love/love.component.ts
+++ src/app/love/love.component.ts
@@ -3,3 +3,24 @@ import { Router, ActivatedRoute, Params } from '@angular/router'
 
 import { DataService } from '../core/data.service'
 import { IFriend, ILove, ILoveToken } from '../shared/interfaces'
+
+@Component({
+    selector: 'app-love',
+    templateUrl: './love.component.html',
+    styleUrls: [ './love.component.css' ]
+})
+
+export class LoveComponent implements OnInit {
+
+    love: ILove[] = []
+    friend: IFriend
+
+    constructor(private dataService: DataService,
+                private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.dataService.getFriend(id).subscribe((friend: IFriend) => {
+            this.friend = friend
+        })
+    }
+}
