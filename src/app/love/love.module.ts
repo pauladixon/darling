5573b76b4c0eccbdf86392c83a40a70a52e648b5
paula dixon <paulapdixon@gmail.com>
2020-10-08 12:13:33 -0700

    set up love module

---
+++ src/app/love/love.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+
+import { SharedModule } from '../shared/shared.module'
+import { LoveComponent } from './love.component'
+import { LoveRoutingModule } from './love-routing.module'
+
+@NgModule({
+    imports: [ CommonModule, FormsModule, SharedModule, LoveRoutingModule ],
+    declarations: [ LoveComponent ]
+})
+
+export class LoveModule {}
